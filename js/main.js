e878c9cbf8ac7990cb12c05dbedfdd6a78bbcdc4
NikitaGolubovich <sctvuser@yandex.ru>
2022-02-07 17:17:03 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let btnOn = document.querySelector(".brands__btn-menu--active");
+let imgOn = document.querySelector(".brands__menu-img");
+let itemOn = document.querySelectorAll(".brands__item--active");
+
+
+btnOn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if (this.textContent === "Подробнее") {
+        this.textContent = "Скрыть";
+        for (let i = 0; i < itemOn.length; i++) {
+            let element = itemOn[i];
+            element.style.display = "block";
+        }
+
+        // imgOn.style.transform = "rotate(180deg)";
+    } else {
+        this.textContent = "Подробнее";
+        for (let i = 0; i < itemOn.length; i++) {
+            let element = itemOn[i];
+            element.style.display = "none";
+        }
+        // imgOn.style.transform = "rotate(180deg)";
+    }
+});
+
+// const swiper = new Swiper(".swiper", {
+//     // Optional parameters
+//     direction: "vertical",
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//     },
+// });
